d53c29b549d949229b72c872f455ac8ec590bdf9
Rabin Kumar Sharma <sharmarabin788@gmail.com>
2026-01-04 19:16:54 +0545

    useFetch hook completed

---
+++ services/useFetch.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autofetch: boolean = true) => {
+
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState<boolean>(autofetch);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            setError(null);
+            const result = await fetchFunction();
+            setData(result);
+        } catch (error) {
+            setError(error instanceof Error ? error : new Error("An error occured"));
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const reset = () => {
+        setData(null);
+        setError(null);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        if (autofetch) {
+            fetchData();
+        }
+    }, [])
+
+    return { data, loading, error, refetch: fetchData, reset };
+
+    
+}
+export default useFetch;
